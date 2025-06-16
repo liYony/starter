e14398b10f4101788129c32a7056a338616a65f3
liYony <941843540@qq.com>
2025-06-16 20:28:17 +0800

    custom configuration

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,7 +4,54 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+--- Common
+map("i", "jj", "<Esc>")
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("n", "gb", "<C-o>", { desc = "jump jump back" })
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+--- <Leader>
+map({ "n", "i", "v" }, "<leader>q", "<cmd> q! <cr>")
+
+--- Editing
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>", { desc = "file save" })
+map({ "n", "i", "v" }, "<C-z>", "<cmd> undo <cr>", { desc = "history undo" })
+map({ "n", "i", "v" }, "<C-y>", "<cmd> redo <cr>", { desc = "history redo" })
+
+--- Comments
+map("n", "<C-_>", "gcc", { desc = "comment toggle", remap = true })
+map("i", "<C-_>", "<Esc>gcc^i", { desc = "comment toggle", remap = true })
+map("v", "<C-_>", "gc", { desc = "comment toggle", remap = true })
+
+--- Search
+map({ "n", "i", "v" }, "<C-f>", function()
+  if vim.bo.filetype == "TelescopePrompt" then
+    vim.cmd "q!"
+  else
+    vim.cmd "Telescope current_buffer_fuzzy_find"
+  end
+end, { desc = "search search in current buffer" })
+map({ "n", "i", "v" }, "<A-f>", function()
+  if vim.bo.filetype == "TelescopePrompt" then
+    vim.cmd "q!"
+  else
+    vim.cmd "Telescope live_grep"
+  end
+end, { desc = "search search across project" })
+map({ "n", "i", "v" }, "<C-p>", function()
+  if vim.bo.filetype == "TelescopePrompt" then
+    vim.cmd "q!"
+  else
+    vim.cmd "Telescope find_files"
+  end
+end, { desc = "search search across project" })
+
+--- Terminal
+map({ "n", "i", "v", "t" }, "<A-v>", function()
+  require("nvchad.term").toggle { pos = "vsp", id = "vtoggleTerm" }
+end, { desc = "terminal toggle vertical term" })
+map({ "n", "i", "v", "t" }, "<A-h>", function()
+  require("nvchad.term").toggle { pos = "sp", id = "htoggleTerm" }
+end, { desc = "terminal toggle horizontal term" })
+map({ "n", "i", "v", "t" }, "<A-i>", function()
+  require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
+end, { desc = "terminal toggle floating term" })
--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,33 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+vim.o.cursorlineopt ='both' -- to enable cursorline!
+
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.shiftwidth = 4          -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 4             -- insert 4 spaces for a tab
+
+--- Enable OSC 52 for copying to system clipboard in SSH
+-- See: https://github.com/neovim/neovim/issues/28611#issuecomment-2147744670
+if os.getenv "SSH_TTY" then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    -- Try to use OSC 52 escape sequences to copy to system clipboard
+    -- It should not break anything if it’s not supported
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy "+",
+      ["*"] = require("vim.ui.clipboard.osc52").copy "*",
+    },
+    -- Disable paste since it does not work in some terminals,
+    -- i.e., xterm.js (many app based on Web tech use this)
+    paste = {
+      ["+"] = function(lines)
+        return vim.split(vim.fn.getreg '"', "\n")
+      end,
+      ["*"] = function(lines)
+        return vim.split(vim.fn.getreg '"', "\n")
+      end,
+    },
+  }
+end
